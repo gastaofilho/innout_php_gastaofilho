9be7069a73f3523b0e982fab87a2d4cacec2bcc7
Gastão <gastaofilho@hotmail.com>
2023-10-17 10:43:33 -0300

    html e css de registro feito

---
+++ public/assets/js/app.js
@@ -0,0 +1,8 @@
+
+(function () {
+    const menuToggle = document.querySelector('.menu-toogle')
+    menuToggle.onclick = function (e) {
+        const body = document.querySelector('body')
+        body.classList.toggle('hide-sidebar')
+    }
+})()
--- src/views/template/left.php
+++ src/views/template/left.php
@@ -9,13 +9,13 @@
             </li>
             <li class="nav-item">
                 <a href="day_records.php">
-                    <i class="icofont-ui-check mr-2"></i>
+                    <i class="icofont-ui-calendar mr-2"></i>
                     Relatório Mensal
                 </a>
             </li>
             <li class="nav-item">
                 <a href=".php">
-                    <i class="icofont-ui-calendar mr-2"></i>
+                    <i class="icofont-chart-histogram mr-2"></i>
                     Relatório Gerencial
                 </a>
             </li>
@@ -27,4 +27,21 @@
             </li>
         </ul>
     </nav>
+    <div class="sidebar-widgets">
+        <div class="sidebar-widget">
+            <i class="icon icofont-hour-glass text-primary"></i>
+            <div class="info">
+                <span class="main text-primary">04:23</span>
+                <span class="label text-muted">Horas Trabalhadas</span>
+            </div>
+        </div>
+        <div class="division my-3"></div>
+        <div class="sidebar-widget">
+            <i class="icon icofont-ui-alarm text-danger"></i>
+            <div class="info">
+                <span class="main text-danger">18:52</span>
+                <span class="label text-muted">Horas de Saída</span>
+            </div>
+        </div>
+    </div>
 </aside>
