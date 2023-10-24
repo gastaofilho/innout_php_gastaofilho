a4cfdbd2090143d38fe393053c4317ee8ea935db
Gastão <gastaofilho@hotmail.com>
2023-10-24 09:27:11 -0300

    relógios ativados

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,3 +6,31 @@
         body.classList.toggle('hide-sidebar')
     }
 })()
+
+
+
+function activateClock() {
+    const activateClock = document.querySelector('[active-clock]')
+    if (!activateClock) return
+
+    function addOneSecond(hours, minutes, seconds) {
+        const d = new Date()
+
+        d.setHours(parseInt(hours))
+        d.setMinutes(parseInt(minutes))
+        d.setSeconds(parseInt(seconds) + 1)
+
+        const h = `${d.getHours()}`.padStart(2, 0)
+        const m = `${d.getMinutes()}`.padStart(2, 0)
+        const s = `${d.getSeconds()}`.padStart(2, 0)
+
+        return `${h}:${m}:${s}`
+    }
+
+    setInterval(function () {
+        const parts = activateClock.innerHTML.split(':')
+        activateClock.innerHTML = addOneSecond(...parts)
+    }, 1000)
+}
+
+activateClock()
